c88c38d4bc1dc6e82d0bd7a57a7197af5f89881e
Pedro Ferreira <plferreira4@gmail.com>
2019-06-28 16:12:38 +0100

    Move the 'rollup' configuration to its own file (rollup.config.js).
    - Typescript compiles initially to the 'source' directory, and then rollup picks that up to generate the 'build' files.
    - Add the 'terser' plugin to also minimize the output files.

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { terser } from 'rollup-plugin-terser';
+
+
+module.exports = {
+    input: 'source/utilities.js',
+    output: [
+        {
+            file: 'build/utilities.cjs.js',
+            format: 'cjs'
+        },
+        {
+            file: 'build/utilities.iife.js',
+            format: 'iife',
+            name: 'Utilities'
+        },
+        {
+            file: 'build/utilities.esm.js',
+            format: 'esm'
+        }
+    ],
+    plugins: [ terser() ]
+};
