1969456f97ba06e0e9f693f79628c77ee47bc9a0
Pedro Ferreira <plferreira4@gmail.com>
2019-08-02 14:42:21 +0100

    Convert more tests to 'jest'.

---
+++ tests/is_array.test.js
@@ -0,0 +1,26 @@
+import * as Utilities from "../source/utilities";
+
+test("Test type.", () => {
+    var values = [
+        { value: 1, expect: false },
+        { value: 1.2, expect: false },
+        { value: NaN, expect: false },
+        { value: Infinity, expect: false },
+        { value: "1.3", expect: false },
+        { value: "a", expect: false },
+        { value: "", expect: false },
+        { value: undefined, expect: false },
+        { value: null, expect: false },
+        { value: true, expect: false },
+        { value: {}, expect: false },
+        { value: function() {}, expect: false },
+        { value: [], expect: true },
+    ];
+
+    for (let a = 0; a < values.length; a++) {
+        const valueInfo = values[a];
+        const result = Utilities.isArray(valueInfo.value);
+
+        expect(result).toBe(valueInfo.expect);
+    }
+});
