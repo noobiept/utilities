519da7db04a5603b669667a5109ec8fc78025e91
Pedro Ferreira <plferreira4@gmail.com>
2019-08-07 12:27:19 +0100

    Improve the 'timeToString()' tests.
    - Define the second/minute/etc once at the top of the file.
    - Use those variables to set the milliseconds values.
    - Test the 'daytime' format as well.

--- tests/time_to_string.test.js
+++ tests/time_to_string.test.js
@@ -1,5 +1,10 @@
 import { timeToString } from "../source/utilities";
 
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
 test("Validate arguments.", () => {
     // no arguments given
     expect(function() {
@@ -12,41 +17,69 @@ test("Validate arguments.", () => {
     }).toThrow();
 
     // passed an invalid 'totalUnits' argument, should default to 2
-    const second = 1000;
-    const minute = 60 * second;
-    const hour = 60 * minute;
-    expect(timeToString({ time: hour + minute + second, units: "ab" })).toBe(
+    expect(timeToString({ time: HOUR + MINUTE + SECOND, units: "ab" })).toBe(
         "1 hour 1 minute"
     );
-    expect(timeToString({ time: hour + minute + second, units: 3 })).toBe(
+    expect(timeToString({ time: HOUR + MINUTE + SECOND, units: 3 })).toBe(
         "1 hour 1 minute 1 second"
     );
 });
 
-test("Test with valid arguments.", () => {
+test("The 'string' and 'daytime' format.", () => {
     const values = [
-        { milliSeconds: 0, expectedString: "0 seconds" },
-        { milliSeconds: 1000, expectedString: "1 second" },
-        { milliSeconds: 30000, expectedString: "30 seconds" },
-        { milliSeconds: 60000, expectedString: "1 minute" },
-        { milliSeconds: 61000, expectedString: "1 minute 1 second" },
-        { milliSeconds: 150000, expectedString: "2 minutes 30 seconds" },
-        { milliSeconds: 3600000, expectedString: "1 hour" },
-        { milliSeconds: 3601000, expectedString: "1 hour 1 second" },
-        { milliSeconds: 3720000, expectedString: "1 hour 2 minutes" },
-        { milliSeconds: 9000000, expectedString: "2 hours 30 minutes" },
-        { milliSeconds: 86400000, expectedString: "1 day" },
-        { milliSeconds: 88201000, expectedString: "1 day 30 minutes" },
-        { milliSeconds: 864020000, expectedString: "10 days 20 seconds" },
+        { time: 0, string: "0 seconds", daytime: "00:00:00" },
+        { time: SECOND, string: "1 second", daytime: "00:00:01" },
+        { time: 30 * SECOND, string: "30 seconds", daytime: "00:00:30" },
+        { time: MINUTE, string: "1 minute", daytime: "00:01:00" },
+        {
+            time: MINUTE + SECOND,
+            string: "1 minute 1 second",
+            daytime: "00:01:01",
+        },
+        {
+            time: 2 * MINUTE + 30 * SECOND,
+            string: "2 minutes 30 seconds",
+            daytime: "00:02:30",
+        },
+        { time: HOUR, string: "1 hour", daytime: "01:00:00" },
+        { time: HOUR + SECOND, string: "1 hour 1 second", daytime: "01:00:01" },
+        {
+            time: HOUR + 2 * MINUTE,
+            string: "1 hour 2 minutes",
+            daytime: "01:02:00",
+        },
+        {
+            time: 2 * HOUR + 30 * MINUTE,
+            string: "2 hours 30 minutes",
+            daytime: "02:30:00",
+        },
+        { time: DAY, string: "1 day", daytime: "1 00:00:00" },
+        {
+            time: DAY + 30 * MINUTE,
+            string: "1 day 30 minutes",
+            daytime: "1 00:30:00",
+        },
+        {
+            time: 10 * DAY + 20 * SECOND,
+            string: "10 days 20 seconds",
+            daytime: "10 00:00:20",
+        },
     ];
 
     for (let a = 0; a < values.length; a++) {
         const value = values[a];
-        const result = timeToString({ time: value.milliSeconds });
+        const stringResult = timeToString({ time: value.time });
+        const daytimeResult = timeToString({
+            time: value.time,
+            format: "daytime",
+        });
 
-        expect(result).toBe(value.expectedString);
+        expect(stringResult).toBe(value.string);
+        expect(daytimeResult).toBe(value.daytime);
     }
+});
 
+test("The 'units' argument.", () => {
     expect(timeToString({ time: 88201000, units: 3 })).toBe(
         "1 day 30 minutes 1 second"
     );
