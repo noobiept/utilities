2238ce221c552ac8d43f7c4848cec5079bb41eae
Pedro Ferreira <plferreira4@gmail.com>
2019-08-19 16:10:53 +0100

    Add additional tests to the 'Dialog' class.

--- tests/dialog.test.js
+++ tests/dialog.test.js
@@ -1,4 +1,4 @@
-import { Dialog } from "../source/utilities";
+import { Dialog, DialogPosition } from "../source/utilities";
 
 beforeEach(() => {
     document.body.innerHTML = "";
@@ -65,6 +65,198 @@ test("If the callback is called.", (done) => {
 
     document.querySelector(".dialogButton").click();
 });
+
+test("The 'position' argument.", () => {
+    const testDialogPosition = (position, dialogCss) => {
+        const dialog = new Dialog({
+            title: "a",
+            body: "b",
+            position,
+        });
+        dialog.open();
+
+        const dialogElement = document.querySelector(".dialog");
+        expect(dialogElement.classList.contains(dialogCss)).toBe(true);
+
+        // clear the previous dialog
+        document.body.innerHTML = "";
+    };
+
+    // default is 'center'
+    testDialogPosition(undefined, "dialog-center");
+
+    // test that when setting the 'position' argument you get the correct css class set
+    const positions = {
+        "dialog-bottomLeft": DialogPosition.bottomLeft,
+        "dialog-bottom": DialogPosition.bottom,
+        "dialog-bottomRight": DialogPosition.bottomRight,
+        "dialog-center": DialogPosition.center,
+    };
+
+    Object.entries(positions).forEach(([key, value]) => {
+        testDialogPosition(value, key);
+    });
+});
+
+test("The '.open()' method.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+    });
+
+    // not added yet
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // added
+    dialog.open();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+
+    // already opened, shouldn't make a difference
+    dialog.open();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+});
+
+test("The '.close()' method.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+    });
+
+    // not added yet
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // added
+    dialog.open();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+
+    // closed
+    dialog.close();
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // already closed, shouldn't make a difference
+    dialog.close();
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+});
+
+test("The '.close()' on a non-modal dialog.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+        modal: false,
+    });
+
+    // not added yet
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // added
+    dialog.open();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+
+    // closed
+    dialog.close();
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+});
+
+test("The '.toggle()' method.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+    });
+
+    // not added yet
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // opened
+    dialog.toggle();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+
+    // closed
+    dialog.toggle();
+    expect(document.querySelector(".dialog")).not.toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(dialog.isOpened()).toBe(false);
+
+    // opened again
+    dialog.toggle();
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(dialog.isOpened()).toBe(true);
+});
+
+test("The '.setTitle()' method.", () => {
+    const dialog = new Dialog({
+        title: "First.",
+        body: "",
+    });
+    dialog.open();
+    expect(document.querySelector(".dialogTitle").innerHTML).toBe("First.");
+
+    dialog.setTitle("Second.");
+    expect(document.querySelector(".dialogTitle").innerHTML).toBe("Second.");
+});
+
+test("The '.setBody()' method.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "First.",
+    });
+    dialog.open();
+    expect(document.querySelector(".dialogBody").innerHTML).toBe("First.");
+
+    dialog.setBody("Second.");
+    expect(document.querySelector(".dialogBody").innerHTML).toBe("Second.");
+});
+
+test("Creating a non-modal dialog.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+        modal: false,
+    });
+    dialog.open();
+
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+});
+
+test("Creating a dialog without the 'ok' button.", () => {
+    const dialog = new Dialog({
+        title: "",
+        body: "",
+        modal: false,
+        okButton: false,
+    });
+    dialog.open();
+
+    expect(document.querySelector(".dialog")).toBeTruthy();
+    expect(document.querySelector(".dialogOverlay")).not.toBeTruthy();
+    expect(document.querySelector(".dialogTitle")).toBeTruthy();
+    expect(document.querySelector(".dialogBody")).toBeTruthy();
+    expect(document.querySelector(".dialog hr")).not.toBeTruthy();
+    expect(document.querySelector(".dialogButtons")).not.toBeTruthy();
+    expect(document.querySelector(".dialogButton")).not.toBeTruthy();
+});
+
 /*
 test("If the dialog is correctly removed after pressing the 'enter' key.", (done) => {
     expect.assertions(3);
