93e3a29d0a468729ebf13baa9a1199aabd940026
Pedro Ferreira <plferreira4@gmail.com>
2025-02-06 22:34:02 +0100

    ⬆️: Upgrade some dependencies.

--- tests/dialog.spec.ts
+++ tests/dialog.spec.ts
@@ -6,39 +6,36 @@ test.describe("Dialog", () => {
     });
 
     test("Should open and close a basic dialog.", async ({ page }) => {
-        const dialog = page.locator(".dialog");
-        await expect(dialog).not.toBeVisible();
+        await expect(page.locator(".dialog")).not.toBeVisible();
 
         await page.locator("#OpenModalDialog").click();
-        await expect(dialog).toBeVisible();
+        await expect(page.locator(".dialog")).toBeVisible();
 
         await page.locator(".dialogButton").click();
-        await expect(dialog).not.toBeVisible();
+        await expect(page.locator(".dialog")).not.toBeVisible();
     });
 
     test("Should close the dialog when the 'escape' key is pressed.", async ({
         page,
     }) => {
-        const dialog = page.locator(".dialog");
-        await expect(dialog).not.toBeVisible();
+        await expect(page.locator(".dialog")).not.toBeVisible();
 
         await page.locator("#OpenModalDialog").click();
-        await expect(dialog).toBeVisible();
+        await expect(page.locator(".dialog")).toBeVisible();
 
         await page.keyboard.press("Escape");
-        await expect(dialog).not.toBeVisible();
+        await expect(page.locator(".dialog")).not.toBeVisible();
     });
 
     test("Should close the dialog when the 'enter' key is pressed.", async ({
         page,
     }) => {
-        const dialog = page.locator(".dialog");
-        expect(dialog).not.toBeVisible();
+        expect(page.locator(".dialog")).not.toBeVisible();
 
         await page.locator("#OpenModalDialog").click();
-        await expect(dialog).toBeVisible();
+        await expect(page.locator(".dialog")).toBeVisible();
 
         await page.keyboard.press("Enter");
-        await expect(dialog).not.toBeVisible();
+        await expect(page.locator(".dialog")).not.toBeVisible();
     });
 });
